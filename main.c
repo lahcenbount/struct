d899f44937e7c632b421383d421b6bf27f086d61
lahcenbont <rachdilahcen2023@gmail.com>
2024-11-25 10:33:10 +0100

    fix a bug

--- main.c
+++ main.c
@@ -4,121 +4,135 @@
 
 
 typedef struct {
-     char rue[45];
-     char ville[45];
-     char codepostal[45];
-} adresse;
+     char titre[45];
+     char description[123];
+     char datecheance[455];
+     char priorte[21];
+} tache;
+    tache add[122];
+    int nombreadd=0;
 
-typedef struct 
-{
-char nom[25];
-int age;
-adresse adr;
-} personne;
-
-personne personnes[100];
-
-personne creerpersonne()
-{
-    personne p;
-
-    printf("tape le  nome:");
-    scanf("%s",p.nom);
-    printf("age:");
-    scanf("%d", &p.age);
-    printf("ville:");
-    scanf("%s",p.adr.ville );
-    printf("tape rue:");
-    scanf("%s",p.adr.rue);
-    printf(" donne Code Postal: "); 
-    scanf("%s",p.adr.codepostal);
-
-    return p;
+void afficheraddition(){
+    printf("\addition:\n");
+    printf("\n1.ajouter un tache\n2.afficher list taches\n3.modifier un taches\n4.supprimer un tache\n5.feltrer les tache priote\n6.qiutter\n");
 }
+void ajoutertache(){
+ tache tac; 
+    printf("donne titre:");
+    scanf("%s",tac.titre);
+    printf("description :");
+    scanf("%s",tac.description);
+    printf("date echeance (nn/jj/mm) :");
+    scanf("%s",tac.datecheance);
+    printf("priorte(high ,low):");
+    scanf("%s",tac.priorte);
+    add[nombreadd]=tac;
+    nombreadd++;
 
-void afficherpersonne(personne p)
-{
-    printf("Nom: %s\n", p.nom);
-    printf("Âge: %d\n", p.age); 
-    printf("Adresse: %s, %s, %s\n", p.adr.rue, p.adr.ville, p.adr.codepostal);
-
+    printf("ajout avec succes.\n");
 }
-void metterajourpersonne(personne *p)
- { 
-    printf("Nouveau nom: ");
- scanf("%s", p->nom);
-  printf("Nouvel âge: "); scanf("%d", &p->age);
-   printf("Nouvelle ville: "); 
-   
-   scanf("%s", p->adr.ville); 
-   printf("Nouvelle rue: ");
-   
- scanf("%s", p->adr.rue); 
- printf("Nouveau code postal: ");
-  scanf("%s", p->adr.codepostal);
- }
-int main() {
-    int n=0 , choix , index;
-    while (1) {
-        printf("\n1. creer une personne\n2. affichr les personnes\n3.suppremir un personne\n4.enter l'index de la personne metter a jour (1 à %d)\n5.quitter\n ");
-        printf("choisissez un option :  ");
-        scanf("%d", &choix);
-        switch (choix) {
+void affichertache(tache tac){
+      printf("donne titre: %s\n" , tac.titre);
+      printf("description: %s\n" , tac.description);
+      printf("datecheance %s\n" , tac.datecheance);
+      printf("priorte %s\n" ,tac.priorte);
+    }
+void afficherlisteadd(){
+    for(int i=0 ; i<nombreadd ; i++){
+        printf("\ntach:%d:/n",i+1);
+        affichertache(add[i]);
+
+    }
+}
+void modifiertache(){
+    int indek;
+    printf("entrez l indek de la tache à modifier (1 à %d):", nombreadd);
+    scanf("%d", &indek);
+    if( indek> 0 && indek <= nombreadd){
+        tache*tac = &add[indek - 1];
+       
+        printf("entre nouveau titre:");
+        scanf("¨%[^n]",tac->titre);
+        printf(" entre nouveau description :");
+         scanf("%s",tac->description);
+        printf("entre nouveau de date echeeance (nn/jj/mm):");
+         scanf("%s",tac->datecheance);
+        printf("entre nouveau de priorte(wigh , low):"); 
+         scanf("%s",tac->priorte);
+        printf("tache modifier avec susses .\n");
+          }
+          else{ printf("indek invalide .\n");
+          }
+          }   
+          void supprimertache(){
+            int  indek ;
+            printf("enter l'indek de la tach à supprimer (1 à %d) : ", nombreadd);
+            scanf("%d",&indek);
+            if( indek>0 && indek <=nombreadd){
+                for(int i=indek -1 ; indek<nombreadd -1 ; i++){
+                    add[i] = add[i+1];
+     }
+     nombreadd-- ;
+     printf("tach supprimer avec susses.\n:");
+
+     }else{
+        printf("indek invalide .\n :");
+     }
+     }
+     void filtreraddparpriorite(){
+    char prior[20];
+    printf("Entrez la priorité pour filtrer (high, low): ");
+    scanf("%s", prior);  
+
+    for(int i = 0; i < nombreadd; i++) {
+        if(strcmp(add[i].priorte, prior) == 0){
+            printf("Tâche %d:\n", i + 1);
+            affichertache(add[i]);
+        }
+    }
+
+     }
+     int main()
+     {
+        int choix ;
+        do {
+        afficheraddition();
+            printf("Choisissez une option : ");
+            scanf("%d", &choix);
+
+            switch(choix) 
+            {
             case 1:
-                personnes[n] = creerpersonne();
-                n++;
-                break;
-            case 2:
-                for ( int i =0 ;i<n ;i++) {
-                    // printf("----------------------------------------\n");
-                    printf("Personne %d:\n", i + 1);
-                    afficherpersonne(personnes[i]);
-                    // printf("----------------------------------------\n\n");
-                }
-                break;
-                case 3:
-                scanf("%d", &index);
-                if (index >= 1 && index <= n){
-                index--;
-    
 
-            for (int i = index ; i < n - 1; i++){
-                personnes[i] =personnes[i + 1];
+              ajoutertache();  
+            break;
+                 case 2:
+                afficherlisteadd() ;
+                
+                break;
+                 case 3:
+                modifiertache();
+                
+                break;
+                 case 4:
+                supprimertache();
+                break;
+                 case 5:
+                filtreraddparpriorite();
+                break;
+                 case 6:
+                printf("quitterb la programme");
+                break;
+                
+            
+            default:
+            printf("choix invalide .\n:");
+           
             }
-           n--;
-        printf("Personne supprimee avec succes");
     
-    }else 
-    {
-          printf("Nume invalide . \n");
-    }
-
+     } while (choix!=6);
                 
-        
-                break;
-                printf("Quitter le programme.\n");
-            
-            printf("Choix invalide.\n");
-                break;
-                case 4:
-                scanf("%d", &index);
-                 if (index > 0 && index <= n){
-                 metterajourpersonne(&personnes[index - 1]);
-                } else
-                {
-                    printf("index valid");
-                }
-                case 5:
-                printf("quitter  le programme.\n");
-                return 0;
-                
-                 default: 
-                 printf("Choix invalide.\n");
+        return 0;
 
-
-            
-            } 
-
-    }
-    return 0;
 }
+
